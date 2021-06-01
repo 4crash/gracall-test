58bc412f50a284920708dfa301f7b0a3e5108fd6
crash <creaturives@gmail.com>
2021-06-01 12:21:06 +0200

    add comment about creating pydantic models

--- pydantic_lib/pydantic_post.py
+++ pydantic_lib/pydantic_post.py
@@ -6,7 +6,7 @@ import pytz
 from pydantic_sqlalchemy import sqlalchemy_to_pydantic
 from db_lib.post_model import PostModel
 
-
+# Create pydantic models from SQlAlchemy declaration
 PostOut = sqlalchemy_to_pydantic(PostModel)
 PostBase = sqlalchemy_to_pydantic(PostModel,exclude=["id","created_at"])
 PostDictT = NewType("PostDictT", List[PostOut])
