bf39de3d4bfc9aeadd8e0b7262c275acd864ae50
crash <creaturives@gmail.com>
2021-06-03 08:12:31 +0200

    update fce asdict to generic

--- db_lib/post_model.py
+++ db_lib/post_model.py
@@ -12,7 +12,7 @@ class PostModel(Base):
     id = Column(Integer,primary_key=True,autoincrement=True)
     title = Column(String(50))
     author = Column(String(50))
-    content = Column(String)
+    content = Column(String(500))
     published_at = Column(DateTime, default=pytz.utc.localize(
         datetime.utcnow().replace(second=0, microsecond=0))) 
     published = Column(Boolean)
@@ -20,18 +20,4 @@ class PostModel(Base):
         datetime.utcnow().replace(second=0, microsecond=0)))
     
     def asdict(self):
-        return {'id': self.id,
-                'title': self.title,
-                'author': self.author,
-                'content': self.content,
-                'published_at': self.published_at,
-                'created_at': self.created_at,
-                'published': self.published}
-    
-    
-
-
-
-
-   
-
+        return self.__dict__
